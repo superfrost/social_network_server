f6d04168192b30689c8a13a9d6ef2f0aae928851
superfrost <otkycu@gmail.com>
2020-05-10 01:48:02 +0300

    add get and post  for post from server

--- index.js
+++ index.js
@@ -196,7 +196,6 @@ function verifyToken(req, res, next) {
     req.token = bearerToken
     next();
   } else {
-                     //res.sendStatus(403)
     console.log("-------No token---------");
     //next();
   }
@@ -238,12 +237,9 @@ app.get('/users', (req, res) => {
     let usersSlice = arr.slice(startPage, endPage)
   
     if (consoleON) {
-      // console.log('*----------------------------------------------*');
-      // console.log('Request query: ', req.query);
       console.log('Page Size: ', pageSize);
       console.log('Current page: ', currentPage);
       console.log('Start slice indx: ', startPage, 'End slice indx: ', endPage);
-      //console.log('Total Users slice', usersSlice);
     }
     return usersSlice
   }
@@ -282,17 +278,6 @@ app.get('/socialSL', (req, res) => {
   )
 })
 
-// http://localhost:5000/social?user_id=5
-app.get('/social', (req, res) => {
-  let userId = req.query.user_id
-  let sqlQuery = `SELECT * FROM users WHERE user_id = ${userId}`
-  return ( 
-    db.all(sqlQuery, (err, row) => {
-      res.json(row)
-      console.log("Send: ",row);
-    })
-  )
-})
 // http://localhost:5000/social?user_id=5
 app.get('/profile/:id', (req, res) => {
   let sqlQuery = "SELECT * FROM users WHERE user_id = ?"
@@ -378,8 +363,7 @@ app.get('/status/:id', (req, res) => {
 
 //! putStatus (update)
 app.put('/status/', (req, res) => {
-  console.log("***PUT***", Date())
-  // console.log("BODY", req.body);
+  console.log("**********PUT**********")
   let sqlQueryRun = "UPDATE users SET status = ? WHERE user_id = 1"
   let params = [req.body.status]
   console.log("Profile id: ", params)
@@ -401,54 +385,106 @@ app.put('/status/', (req, res) => {
   })
 })
 
-let posts = [
-  {id: 1, person_id: 1, date: '15-03-2020 17:13', message: "Hi it's me !!!", like_count: 17},
-  {id: 2, person_id: 2, date: '14-03-2020 16:07', message: "Where are you?", like_count: 4},
-  {id: 3, person_id: 3, date: '13-03-2020 09:57', message: "Are you kidding me?", like_count: 55},
-  {id: 4, person_id: 4, date: '12-03-2020 13:49', message: "There were white out conditions in the town; subsequently, the roads were impassable.", like_count: 32},
-  {id: 5, person_id: 5, date: '11-03-2020 11:45', message: "There's a reason that roses have thorns.", like_count: 43},
-  {id: 6, person_id: 6, date: '10-03-2020 20:45', message: "Whenever he saw a red flag warning at the beach he grabbed his surfboard.", like_count: 9},
-  {id: 7, person_id: 7, date: '09-03-2020 13:36', message: "Choosing to do nothing is still a choice, after all.", like_count: 13},
-  {id: 8, person_id: 8, date: '08-03-2020 12:20', message: "She saw no irony asking me to change but wanting me to accept her for who she is.", like_count: 11},
-  {id: 9, person_id: 2, date: '07-03-2020 15:41', message: "Toddlers feeding raccoons surprised even the seasoned park ranger.", like_count: 79},
-  {id: 10, person_id: 4, date: '06-03-2020 18:43', message: "She finally understood that grief was her love with no place for it to go.", like_count: 23},
-  {id: 11, person_id: 3, date: '05-03-2020 13:20', message: "The pigs were insulted that they were named hamburgers.", like_count: 45},
-  {id: 12, person_id: 6, date: '04-03-2020 16:17', message: "He used to get confused between soldiers and shoulders, but as a military man, he now soldiers responsibility.", like_count: 22},
-  {id: 13, person_id: 7, date: '03-03-2020 15:00', message: "Yeah, I think it's a good environment for learning English.", like_count: 56},
-  {id: 14, person_id: 1, date: '02-03-2020 11:10', message: "Nobody questions who built the pyramids in Mexico.", like_count: 73},
-  {id: 15, person_id: 8, date: '01-03-2020 13:40', message: "When he encountered maize for the first time, he thought it incredibly corny.", like_count: 5},
-  {id: 16, person_id: 6, date: '20-02-2020 13:40', message: "Hey Lera", like_count: 34},
-]
-
 //! Get User Posts
 app.get("/posts/:id", (req, res) => {
-  console.log("***GET Posts***", Date.now())
+  console.log("***GET Posts***")
   let user_id = req.params.id
-  if (!user_id)  {
-    res.status(401).json({resultCode: 1, error: "No or wrong user ID"})}
-  else { 
-    let userPosts = posts.filter(p => p.person_id == user_id)
-  res.json(userPosts)
-  console.log(userPosts);
-  }
-
+  if (!user_id)  {    
+    return res.status(401).json({
+      resultCode: 1, 
+      error: "No or wrong user ID"})}
+    let sqlQuery = "SELECT * FROM posts WHERE person_id = ?"
+    db.all(sqlQuery, user_id, (err, result) => {
+      if (err) {
+        res.status(400).json({
+          resultCode: 1,
+          "error":err.message});
+        console.log(err.message)
+        return;
+      }
+      let message = {
+        resultCode: 0,
+        message: "OK here is your Posts",
+        posts: result,
+      }
+      res.json(message)
+      console.log(message);
+    })
 })
+//! Add new Post
+app.post("/posts", (req, res) => {
+  if (req.user.message.user_id.toString() === req.body.user_id.toString()) {
+    console.log("***********ADD POST**********");
+    db.serialize(() => {
+      let params = [req.user.message.user_id, req.body.post];
+      let sqlQueryRun = "INSERT INTO posts (person_id, date, message) VALUES (?, datetime('now'), ?)";
+      db.run(sqlQueryRun, params, function (err) {
+        if (err) {
+          res.status(400).json({
+            resultCode: 1,
+            error: err.message,
+          });
+          console.log(err.message);
+          return;
+        }
+        console.log("Last changed ID: ", this.lastID);
+        let lastId = this.lastID;
+        db.get("SELECT * FROM posts WHERE id = ?", lastId, (err, row) => {
+          if (err) {
+            res.status(400).json({
+              resultCode: 1,
+              error: err.message,
+            });
+            console.log(err.message);
+            return;
+          }
+          let message = {
+            resultCode: 0,
+            message: "OK here is your Posts",
+            data: row,
+          };
+          res.json(message);
+          console.log(message);
+        });
+      })
+    });
+  }
+});
 
-//! Use datetime('now') to generate time stamp in messages
-// Login form response
-// app.post('/login', (req, res) => {
-//   console.log("****POST****", Date());
-//   console.log(req.query);
+let friends = [
+  {id: 1, name: 'Anton', photoSrc: 'https://www.famousbirthdays.com/faces/clooney-george-image.jpg'},
+  {id: 2, name: 'Lera', photoSrc: 'https://www.famousbirthdays.com/headshots/zoe-saldana-5.jpg'},
+  {id: 3, name: 'Nick', photoSrc: 'https://www.famousbirthdays.com/faces/dicaprio-l-image.jpg'},
+  {id: 4, name: 'Ben', photoSrc: 'https://www.famousbirthdays.com/headshots/ben-stiller-4.jpg'},
+  {id: 5, name: 'Kate', photoSrc: 'https://www.famousbirthdays.com/faces/banks-tyra-image.jpg'},
+  {id: 6, name: 'John', photoSrc: 'https://www.famousbirthdays.com/headshots/dwayne-johnson-9.jpg'},
+  {id: 7, name: 'B.Kingsley', photoSrc: 'https://www.famousbirthdays.com/thumbnails/kingsley-ben-large.jpg'},
+  {id: 8, name: 'Will Smit', photoSrc: 'https://www.famousbirthdays.com/headshots/will-smith-1.jpg'}
+]
 
-//   let loginMessage = {
-//     resultCode: 0,
-//     messages: [],
-//     data: {
-//       userId: 1
-//     }
-//   }
-//   res.json(loginMessage)
-// });
+app.post("/friends", (req, res) => {
+  console.log("**********FRIENDS*************")
+  let params = req.body.friends
+  console.log("params: ", params);
+  
+  let sqlQuery = `SELECT * FROM users WHERE user_id IN (${params})`
+  console.log("sqlQuery: ", sqlQuery);
+
+  db.all(sqlQuery, (err, row) => {
+    if(err) {
+      return res.status(400).json({
+        resultCode: 1, 
+        error: err.message})
+    }
+    let message = {
+      resultCode: 0,
+      message: "OK here is your Friends",
+      data: row,
+    };
+    res.json(message);
+    console.log(message);
+  })
+})
 
 app.listen(5000, () => {
     console.log("Listening on http://localhost:5000")
